a8de5aafb20061880ad0f5d0f5a0b21c1e88a5d9
Yanna01santos <135857226+Yanna01santos@users.noreply.github.com>
2024-06-27 23:08:32 -0300

    atualizadoo

--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 3000
+ const express = require('express');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+ const app = express();
 
-app.get('/rota', (req, res) => {
-  res.send('Yaninha do coração!')
-})
+ app.listen(3000,function(){
+console.log('server running');
 
-app.use(express.json()) // Parseia o corpo da requisição JSON
-
-app.post('/', (req, res) => {
-  // Manipulador de requisição POST
-  // ...
-  res.send('Cadastro realizado com sucesso!')
-})
-
-app.listen(port, () => {
-  console.log(`App online na porta: http://localhost:${port}/`)
-})
+ });
+ 
